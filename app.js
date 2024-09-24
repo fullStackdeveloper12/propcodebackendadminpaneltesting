a4c305fc18730c456291986c4c8f545db87c3be9
ltsmeShubham12 <shubham.sharma@amoghbuildtech.com>
2024-09-24 16:31:36 +0530

    cors changed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://propcode-frontenddd.vercel.app"],
     credentials: true,
   })
 );
